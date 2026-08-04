bf04e400f7c715b5e2f4ab003df2aaa5ce6cfa4e
pratikkolhe28 <pratikkolhe85@gmail.com>
2026-08-04 16:57:05 +0530

    Time: 4 ms (66.06%), Space: 94 MB (96.36%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,26 +10,66 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode temp = head;
-        ArrayList<Integer> values = new ArrayList<>();
-
-        while(temp != null) {
-            values.add(temp.val);
-            temp = temp.next;
+        if (head == null || head.next == null) {
+            return true;
         }
 
-        int left = 0;
-        int right = values.size() - 1;
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while(left < right) {
-            if(!values.get(left).equals(values.get(right))) {
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        ListNode temp = slow;
+        ListNode prev = null;
+
+        while(temp != null) {
+            ListNode front = temp.next;            
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+
+        ListNode h1 = head;
+        ListNode h2 = prev;
+
+        while(h2 != null) {
+            if(h1.val != h2.val) {
                 return false;
             }
 
-            left++;
-            right--;
+            h1 = h1.next;
+            h2 = h2.next;
         }
 
         return true;
     }
 }
+
+// ListNode temp = head;
+//         ArrayList<Integer> values = new ArrayList<>();
+
+//         while(temp != null) {
+//             values.add(temp.val);
+//             temp = temp.next;
+//         }
+
+//         int left = 0;
+//         int right = values.size() - 1;
+
+//         while(left < right) {
+//             if(!values.get(left).equals(values.get(right))) {
+//                 return false;
+//             }
+
+//             left++;
+//             right--;
+//         }
+
+//         return true;
