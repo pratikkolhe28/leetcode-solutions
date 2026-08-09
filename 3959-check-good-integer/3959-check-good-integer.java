6cc878fa41473c9d29429b8aaa46353f2677d32f
pratikkolhe28 <pratikkolhe85@gmail.com>
2026-08-10 00:14:13 +0530

    Time: 1 ms (95.51%), Space: 42.4 MB (83.98%) - LeetHub

--- 3959-check-good-integer/3959-check-good-integer.java
+++ 3959-check-good-integer/3959-check-good-integer.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean checkGoodInteger(int n) {
-        int m = n;
         int squareSum = 0;
         int digitSum = 0;
 
         while(n > 0) {
             int rem = n % 10;
-            digitSum = digitSum + rem;
-            squareSum = squareSum + (rem*rem);
-            n = n / 10;
+            digitSum += rem;
+            squareSum += (rem*rem);
+            n /= 10;
         }
 
         if(squareSum-digitSum >= 50) {
