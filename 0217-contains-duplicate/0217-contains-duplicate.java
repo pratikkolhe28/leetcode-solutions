b3ca961133d670199d8cb70ad547a3877661552f
pratikkolhe28 <pratikkolhe85@gmail.com>
2026-08-11 16:18:45 +0530

    Time: 21 ms (35.17%), Space: 107.9 MB (48.78%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,17 +1,35 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
 
-        for(int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if(entry.getValue() != 1) {
+        for(int num : nums) {
+            if(set.contains(num)) {
                 return true;
             }
+
+            set.add(num);
         }
 
         return false;
     }
 }
+
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         for(int i = 0; i < nums.length; i++) {
+//             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+//         }
+
+//         for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//             if(entry.getValue() != 1) {
+//                 return true;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
+
