8ec93017d1e7bf38b6474c03c6088beb817aab2b
pratikkolhe28 <pratikkolhe85@gmail.com>
2026-08-16 12:03:40 +0530

    Time: 5 ms (13.14%), Space: 44 MB (5.6%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isValid(String s) {
+        if (s.length() % 2 != 0)
+            return false;
+            
         Deque<Character> stack = new ArrayDeque<>();
 
         for(int i = 0; i < s.length(); i++) {
