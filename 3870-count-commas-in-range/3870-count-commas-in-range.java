59bfaa19b04788cae6dd1f777fb004619f2cc06a
pratikkolhe28 <pratikkolhe85@gmail.com>
2026-09-08 20:04:15 +0530

    Time: 1 ms (83.65%), Space: 42.8 MB (8.32%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countCommas(int n) {
+        if(n < 1000) {
+            return 0;
+        } else {
+            n -= 1000;
+        }
+
+        return n+1;
+    }
+}
