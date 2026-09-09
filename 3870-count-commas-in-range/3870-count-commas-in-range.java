79801a47a4942741080d81aa13540637090ed3e0
pratikkolhe28 <pratikkolhe85@gmail.com>
2026-09-09 20:03:29 +0530

    Time: 1 ms (83.58%), Space: 42.3 MB (85.34%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,9 +1,31 @@
 class Solution {
     public int countCommas(int n) {
+        long commas = 0;
+
         if(n < 1000) {
-            return 0;
+            commas = 0;
+        } else if (n <= 999999) {
+            commas += n - 999;
+        } else if (n <= 999999999) {
+            commas += n - 999;
+            commas += (n - 999999) * 2;
+        } else if (n <= 999999999999L) {
+            commas += n - 999;
+            commas += (n - 999999) * 2;
+            commas += (n - 999999999) * 3;
+        } else if (n <= 999999999999999L) {
+            commas += n - 999;
+            commas += (n - 999999) * 2;
+            commas += (n - 999999999) * 3;
+            commas += (n - 999999999999L) * 4;
+        } else if (n <= 999999999999999999L) {
+            commas += n - 999;
+            commas += (n - 999999) * 2;
+            commas += (n - 999999999) * 3;
+            commas += (n - 999999999999L) * 4;
+            commas += (n - 999999999999999L) * 5;
         }
 
-        return n - 999;
+        return (int) commas;
     }
 }
